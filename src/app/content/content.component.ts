51449816122aa3cc547981457c4dd031ac5f900d
aurea10 <srta_ferr@hotmail.com>
2019-10-02 22:28:59 -0300

    Animacao

---
+++ src/app/content/content.component.ts
@@ -0,0 +1,35 @@
+import { Component, OnInit } from '@angular/core';
+import { from } from 'rxjs';
+import Typed from 'typed.js';
+
+@Component({
+  
+  selector: 'has-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+
+    const options = {
+      stringsElement: '#typed-strings',
+      strings: ['ETEC Banking', 'Peça a portabilidade hoje mesmo', 'Inovavor, digital e seguro'],
+      typeSpeed: 100,
+      backSpeed: 100,
+      backDelay: 200,
+      smartBackspace: true,
+      fadeOut: true,
+      showCursor: false,
+      startDelay: 1000,
+      loop: true
+    };
+
+    const typed = new Typed('.typing-element', options);
+
+
+  }
+
+}
